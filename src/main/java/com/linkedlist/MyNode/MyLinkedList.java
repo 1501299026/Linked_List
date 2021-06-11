fd2f27f0289c826d57110a78ea0d6f45f43e9dce
1501299026 <sinumsd98@gmail.com>
2021-06-11 04:33:20 -0700

    ADDED: Append value of linkedlist

---
+++ src/main/java/com/linkedlist/MyNode/MyLinkedList.java
@@ -0,0 +1,65 @@
+package com.linkedlist.MyNode;
+
+public class MyLinkedList {
+    public INode head;
+    public INode tail;
+
+    /**
+     * Default constructor assigned both head and tail as null.
+     */
+    public MyLinkedList() {
+        this.head = null;
+        this.tail = null;
+    }
+
+    /**
+     * this method will add nodes.
+     * added if condition to check tail or head is null...then it'll check the next.
+     * @param newNode
+     */
+    public void add(INode newNode) {
+        if (this.tail == null) {
+            this.tail = newNode;
+        }
+        if (this.head == null) {
+            this.head = newNode;
+        } else {
+            INode tempNode = this.head;
+            this.head = newNode;
+            this.head.setNext(tempNode);
+        }
+    }
+
+    /**
+     * added append method to append values in the linked list.
+     * @param myNode
+     */
+    public void append(INode myNode) {
+        if (this.head == null) {
+            this.head = myNode;
+        }
+        if (this.tail == null) {
+            this.tail = myNode;
+        } else {
+            this.tail.setNext(myNode);
+            this.tail = myNode;
+        }
+    }
+    /**
+     * this will print the node.
+     */
+    public void printMyNodes() {
+        StringBuffer myNodes = new StringBuffer("My Nodes: ");
+        INode tempNode = head;
+        while (tempNode.getNext() != null) {
+            myNodes.append(tempNode.getKey());
+            if (!tempNode.equals(tail)) myNodes.append("->");
+            tempNode = tempNode.getNext();
+        }
+        myNodes.append(tempNode.getKey());
+        System.out.println(myNodes);
+    }
+
+
+
+}
